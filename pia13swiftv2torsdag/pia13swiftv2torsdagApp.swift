cc3be89738012c724008ecdb90d5ce0b889303a3
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-11-14 09:16:21 +0100

    Initial Commit

---
+++ pia13swiftv2torsdag/pia13swiftv2torsdagApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia13swiftv2torsdagApp.swift
+//  pia13swiftv2torsdag
+//
+//  Created by Andrés Martinez-Losa on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct pia13swiftv2torsdagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pia13swiftv2torsdagTests/pia13swiftv2torsdagTests.swift
@@ -0,0 +1,17 @@
+//
+//  pia13swiftv2torsdagTests.swift
+//  pia13swiftv2torsdagTests
+//
+//  Created by Andrés Martinez-Losa on 2024-11-14.
+//
+
+import Testing
+@testable import pia13swiftv2torsdag
+
+struct pia13swiftv2torsdagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
